aa03e4459eb7c0fb1c751326552d86d7943f31e1
Fernando Bueno <nando_silverz@hotmail.com>
2023-05-21 21:46:00 -0300

    Adicionado função com extração de links usando regex

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import fs from "fs";
 import chalk from "chalk";
 
+//Função que utiliza expressão regular e o operador de espalhamento ... para extrair links entre [] e ().
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}));
+  return resultados;
+}
+
+//Função que exibe um erro no console caso o arquivo não seja encontrado.
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, "Arquivo não encontrado"));
 }
 
+//Função assíncrona que percorre o arquivo especificado.
 async function pegaArquivo(caminhoDoArquivo) {
-  try{
+  try {
     const encoding = "utf-8";
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
   } catch (erro) {
     trataErro(erro);
   }
